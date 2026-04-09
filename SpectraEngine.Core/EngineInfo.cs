0353cd9339b2c2aa282225d4769aad8211d633bf
D244 <contact@davicloud.net>
2026-04-09 13:53:51 +0200

    Recreate project and add EngineInfo

---
+++ SpectraEngine.Core/EngineInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace SpectraEngine.Core
+{
+    public static class EngineInfo
+    {
+        // const fields for versioning and other build-time engine info
+        public const int MajorVersion = 1;
+        public const int MinorVersion = 0;
+        public const int RevisionVersion = 0;
+        public static readonly string VersionString = $"{MajorVersion}.{MinorVersion}.{RevisionVersion}";
+
+        // Internal engine versioning
+        public const int ModelFormatVersion = 1;
+        public const int MapFormatVersion = 1;
+    }
+}
