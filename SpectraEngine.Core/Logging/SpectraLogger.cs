604ccd818c1733e36f31119f8068e62b1061441e
D244 <contact@davicloud.net>
2026-04-13 10:43:17 +0200

    sync push

---
+++ SpectraEngine.Core/Logging/SpectraLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpectraEngine.Core.Logging
+{
+    /// <summary>
+    /// Spectra engine's logger class. Uses Serilog
+    /// </summary>
+    public class SpectraLogger
+    {
+
+    }
+}
